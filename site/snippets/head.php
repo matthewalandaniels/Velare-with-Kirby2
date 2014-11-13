3f542abe9778a9c87eb45a738a6037c0bcf83013
Matthew Daniels <matthewdaniels@Matthews-MacBook-Pro-2.local>
2014-11-12 20:17:19 -0800

    before iPad safari ios7 debug

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -3,7 +3,7 @@
     <title>VELARE</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0 user-scalable = no">
     <?php echo css('assets/css/styles.css') ?>
     <link href='http://fonts.googleapis.com/css?family=Libre+Baskerville' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
